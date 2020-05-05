2b9fc0801abc95661af6ee497d0d94c30b24cbfc
Tommy White <tommywhite525@gmail.com>
2020-05-05 17:22:45 -0400

    Changed interrupt to switch statement. Transmission stops after continuous 1s as well as 0s now.

--- VIPColorSensorInterruptManchester/VIPColorSensorInterruptManchester.ino
+++ VIPColorSensorInterruptManchester/VIPColorSensorInterruptManchester.ino
@@ -15,6 +15,16 @@
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X);
 
+//Enum describes the different modes the sensor can be in
+enum mode {
+  OFF,
+  HANDSHAKE,
+  SYNC_STOP,
+  SYNC_START,
+  MESSAGE
+};
+int mode = OFF;
+
 //global variables
 int ind = 0;                //samples since last bit
 int arr_size;               //samples per bit
@@ -26,6 +36,7 @@ int bits = 0;               //number of transmitted bits since last sync
 int manchBit = 0;           //index of manchester array
 int manchester[2];          //array used to determine transition for manchester encoding
 int continuous0s = 0;       //used to check if transmission has stopped
+int continuous1s = 0;       //used to check if transmission has stopped
 int lastSum = 0;            //handshakeSum last time the bulb changed 
 boolean started = false;    //true if handshake is over and not in start or stop bit state
 boolean handshake = false;  //true if in handshake
@@ -89,95 +100,32 @@ void setup() {
 
 //Timer1 interrupt, set to FREQ
 ISR(TIMER1_COMPA_vect) {
-  if (started) {
-    if (colorSum >= MINIMUM) {
-      sum++;
-    }
-    //When the determined number of samples/bit have been taken,
-    //the bit is considered a 1 if sum is greater than thresh, 0 otherwise
-    ind = (ind + 1) % arr_size;
-    if (ind == 0) {
-      //The value of the bit is added to the manchester array
-      if (sum >= thresh) {
-        manchester[manchBit] = 1;
-      } else {
-        manchester[manchBit] = 0;
+  switch(mode) {
+    //Off Mode. The sensor waits for the handshake to occur.
+    //Handshake mode is entered when the bulb first changes state.
+    case OFF:
+      if (colorSum >= MINIMUM) {
+        mode = HANDSHAKE;
       }
-      //When the array is full, we determine the data of the past two bits
-      manchBit = (manchBit + 1) % 2;
-      if (manchBit == 0) {
-        //The data is 1 if we transitioned from 0 to 1
-        if (manchester[0] == 0 && manchester[1] == 1) {
-          Serial.println("1");
-          continuous0s = 0;
-        //The data is 0 if we transitioned from 1 to 0
-        } else if (manchester[0] == 1 && manchester[1] == 0) {
-          Serial.println("0");
-          continuous0s = 0;
-        //When we stay low, we guess 0 and check if transmission has stopped
-        } else if (manchester[0] == 0 && manchester[1] == 0) {
-          Serial.println("0");
-          continuous0s++;
-          if (continuous0s >=3) {
-            while(1);
-          }
-        //When we stay high, we guess 1
-        } else {
-          Serial.println("1");
-          continuous0s = 0;
-        }
-      }
-      //Every 16th transmitted bit (8 bits of data), sync mode is entered
-      bits = (bits + 1) % 16;
-      if (bits == 0) {
-        started = false;
-        syncStop = true;
-      }
-      sum = 0;
-    }
-  }
-  //Start bit. We are resynced once we enter this mode,
-  //so we wait for the determined number of samples/bit
-  //and then move back to started mode.
-  else if (syncStart) {
-    sync = (sync+1) % arr_size;
-    if (sync == 0){
-      syncStart = false;
-      started = true;
-    }
-  }
-  //Stop bit. Move on to start bit once light is turned off
-  //and sync is greater than half the determined samples/bit.
-  //The second condition stops us from entering stop mode
-  //if we enter start mode while the bulb is still off
-  //and the stop bit has not yet appeared
-  else if (syncStop) {
-    sync++;
-    if (colorSum < MINIMUM && sync > arr_size/2) {
-      syncStop = false;
-      syncStart = true;
-      sync = 0;
-    }
-  }
-  //Handshake mode. Used to determine the correct samples/bit
-  else if (handshake) {
-    switch(handshakeSwitch) {
+      break;
+    //Handshake mode. Used to determine the correct samples/bit
+    case HANDSHAKE:
       //The bulb is on. When it turns off, we increase handshakeStage
       //changing handshakeSwitch
-      case 0:
-        //We also checked that handshakeSum has incremented enough
+      if (handshakeSwitch == 0) {
+        //We also check that handshakeSum has incremented enough
         //since handshakeSwitch changed to avoid immediate switches
         if (colorSum < MINIMUM && handshakeSum > lastSum + 3) {
           handshakeStage++;
           handshakeSwitch = handshakeStage % 2;
           lastSum = handshakeSum;
         }
-        break;
+      }
       //The bulb is off. When it turns on, we increase handshakeStage,
       //changing handshakeSwitch. If handshakeStage is greater than
       //HANDSHAKE_LEN when the bulb is turned on, the hand shake is
       //over and we enter Stop bit mode.
-      case 1:
+      else {
         if (colorSum >= MINIMUM && handshakeSum > lastSum + 3) {
           handshakeStage++;
           handshakeSwitch = handshakeStage % 2;
@@ -187,20 +135,87 @@ ISR(TIMER1_COMPA_vect) {
             thresh = (int) round(((float)arr_size) / 2);
             Serial.println(handshakeSum);
             Serial.println(arr_size);
-            syncStop = true;
-            handshake = false;
+            mode = SYNC_STOP;
           }
         }
-        break;
-    }
-    //handshakeSum is incremented every interrupt in handshake mode
-    handshakeSum++;
-  }
-  else {
-    //Handshake mode is entered when the bulb first turns on.
-    if (colorSum >= MINIMUM) {
-      handshake = true;
-    }
+      }
+      //handshakeSum is incremented every interrupt in handshake mode
+      handshakeSum++;
+      break;
+    //Stop bit. Move on to start bit once light is turned off
+    //and sync is greater than half the determined samples/bit.
+    //The second condition stops us from entering stop mode
+    //if we enter start mode while the bulb is still off
+    //and the stop bit has not yet appeared
+    case SYNC_STOP:
+      sync++;
+      if (colorSum < MINIMUM && sync > arr_size/2) {
+        mode = SYNC_START;
+        sync = 0;
+      }
+      break;
+    //Start bit. We are resynced once we enter this mode,
+    //so we wait for the determined number of samples/bit
+    //and then move back to started mode.
+    case SYNC_START:
+      sync = (sync+1) % arr_size;
+      if (sync == 0){
+        mode = MESSAGE;
+      }
+      break;
+    case MESSAGE:
+      if (colorSum >= MINIMUM) {
+        sum++;
+      }
+      //When the determined number of samples/bit have been taken,
+      //the bit is considered a 1 if sum is greater than thresh, 0 otherwise
+      ind = (ind + 1) % arr_size;
+      if (ind == 0) {
+        //The value of the bit is added to the manchester array
+        if (sum >= thresh) {
+          manchester[manchBit] = 1;
+        } else {
+          manchester[manchBit] = 0;
+        }
+        //When the array is full, we determine the data of the past two bits
+        manchBit = (manchBit + 1) % 2;
+        if (manchBit == 0) {
+          //The data is 1 if we transitioned from 0 to 1
+          if (manchester[0] == 0 && manchester[1] == 1) {
+            Serial.println("1");
+            continuous0s = 0;
+            continuous1s = 0;
+          //The data is 0 if we transitioned from 1 to 0
+          } else if (manchester[0] == 1 && manchester[1] == 0) {
+            Serial.println("0");
+            continuous0s = 0;
+            continuous1s = 0;
+          //When we stay low, we guess 0 and check if transmission has stopped
+          } else if (manchester[0] == 0 && manchester[1] == 0) {
+            Serial.println("0");
+            continuous0s++;
+            if (continuous0s >=3) {
+              mode = OFF;
+            }
+            continuous1s = 0;
+          //When we stay high, we guess 1
+          } else {
+            Serial.println("1");
+            continuous1s++;
+            if (continuous1s >=3) {
+              mode = OFF;
+            }
+            continuous0s = 0;
+          }
+        }
+        //Every 16th transmitted bit (8 bits of data), sync mode is entered
+        bits = (bits + 1) % 16;
+        if (bits == 0) {
+          mode = SYNC_STOP;
+        }
+        sum = 0;
+      }
+      break;
   }
 }
 
